a5f4240d466610389b83173f2ebe7010cb9f5336
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-01-03 22:49:10 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240103/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void Add(int a,int b){
+    cout<<a<<" + "<<b<<" = "<<a+b;
+}
+void Sub(int a,int b){
+    cout<<a<<" - "<<b<<" = "<<a-b;
+}
+void Mul(int a,int b){
+    cout<<a<<" * "<<b<<" = "<<a*b;
+}
+void Div(int a,int b){
+    cout<<a<<" / "<<b<<" = "<<a/b;
+}
+int main() {
+    int a,b;
+    char c;
+    cin >>a>>c>>b;
+    if(c=='+'){
+        Add(a,b);
+    }
+    else if(c=='-'){
+        Sub(a,b);
+    }
+    else if(c=='*'){
+        Mul(a,b);
+    }
+    else if(c=='/'){
+        Div(a,b);
+    }
+    return 0;
+}
