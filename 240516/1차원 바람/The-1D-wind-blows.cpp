9630e10a580d08c1bbddce6bae69ecf3374d41d5
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-05-16 17:09:10 +0900

    Title: 1차원 바람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/1차원 바람/The-1D-wind-blows.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+
+using namespace std;
+
+
+int arr[100][100];
+int N, M, Q, r;
+char d;
+
+void shiftLeft(int r) {
+  int lastElement = arr[r - 1][M - 1];
+
+  for (int i = M - 1; i > 0; i--) {
+    arr[r - 1][i] = arr[r - 1][i - 1];
+  }
+  arr[r - 1][0] = lastElement;
+} //r번째 줄 오른쪽으로 shift
+
+void shiftRight(int r){
+  int firstElement = arr[r - 1][0];
+
+  for (int i = 0; i < M; i++) {
+    arr[r - 1][i - 1] = arr[r - 1][i];
+  }
+  arr[r - 1][M - 1] = firstElement;
+} //r번째 줄 왼쪽으로 shift
+
+bool upexist(int a) {
+  for (int i = 0; i < M; i++) {
+      if (arr[a-1][i] == arr[a - 2][i]) {
+        return true;
+      }
+    
+  }
+  return false;
+}
+
+bool downexist(int a){
+  for (int i = 0; i < M; i++) {
+      if (arr[a-1][i] == arr[a][i]) {
+        return true;
+      }
+  }
+  return false;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> N >> M >> Q;
+  for (int i = 0; i < N; i++){
+    for (int j = 0; j < M; j++){
+      cin >> arr[i][j];
+    }
+  }
+
+  for (int i = 0; i < Q; i++){
+    cin >> r >> d;
+    int x = r;
+    char dir = d;
+    if (dir == 'L') {
+      shiftLeft(x);
+      while(x > 1){
+        if(upexist(x)) {
+          x--;
+          if (dir == 'L') {
+            shiftRight(x);
+            dir = 'R';
+          } else {
+            shiftLeft(x);
+            dir = 'L';
+          }
+        }
+        else{
+          break;
+        }
+      }
+
+      dir = d;
+      x = r;
+      while (x < N) {
+        if (downexist(x)) {
+          x++;
+          if (dir == 'L') {
+            shiftRight(x);
+            dir = 'R';
+          } else {
+            shiftLeft(x);
+            dir = 'L';
+          }
+        } else {
+          break;
+        }
+      }
+    }
+
+    else if (dir == 'R') {
+      shiftRight(x);
+      while(x > 0){
+        if(upexist(x)) {
+          x--;
+          if (dir == 'L') {
+            shiftRight(x);
+            dir = 'R';
+          } else {
+            shiftLeft(x);
+            dir = 'L';
+          }
+        }
+        else{
+          break;
+        }
+      }
+      dir = d;
+      x = r;
+      while (x < N - 1) {
+        if(downexist(x)) {
+          x++;
+          if (dir == 'L') {
+            shiftRight(x);
+            dir = 'R';
+          } else {
+            shiftLeft(x);
+            dir = 'L';
+          }
+        }
+        else{
+          break;
+        }
+      }
+    }
+  }
+  for (int i = 0; i < N; i++){
+    for (int j = 0; j < M; j++){
+      cout << arr[i][j] << " ";
+    }
+    cout << '\n';
+  }
+}
