8683b683ec853dd3eb6f86dffcb69c685e1f21bb
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-05-18 18:42:43 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240518/방향에 맞춰 이동/move-in-direction.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int arr[101][101];
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+
+int main() {
+    int N;
+    char a;
+    cin>>N;
+    int x = 0;
+    int y = 0;
+    for(int i = 0; i<N; i++){
+        int b;
+        cin>>a;
+        if(a=='N'){
+            cin>>b;
+            for(int i=0;i<b; i++){
+                x = x + dx[0];
+                y = y + dy[0];
+            }
+        }
+        else if(a=='E'){
+            cin>>b;
+            for(int i=0;i<b; i++){
+                x = x + dx[1];
+                y = y + dy[1];
+            }
+        }
+        else if(a=='S'){
+            cin>>b;
+            for(int i=0;i<b; i++){
+                x = x + dx[2];
+                y = y + dy[2];
+            }
+        }
+        else if(a=='E'){
+            cin>>b;
+            for(int i=0;i<b; i++){
+                x = x + dx[3];
+                y = y + dy[3];
+            }
+        }
+    }
+    cout<<y<<" "<<x;
+    return 0;
+}
