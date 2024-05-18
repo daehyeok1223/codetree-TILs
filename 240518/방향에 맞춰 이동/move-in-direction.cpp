09410bca13aa213b3481a47235d71921c3b9407b
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-05-18 18:47:45 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240518/방향에 맞춰 이동/move-in-direction.cpp
+++ 240518/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int arr[101][101];
-
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
@@ -19,32 +17,32 @@ int main() {
         if(a=='N'){
             cin>>b;
             for(int i=0;i<b; i++){
-                x = x + dx[0];
-                y = y + dy[0];
-            }
-        }
-        else if(a=='E'){
-            cin>>b;
-            for(int i=0;i<b; i++){
                 x = x + dx[1];
                 y = y + dy[1];
             }
         }
-        else if(a=='S'){
+        else if(a=='E'){
             cin>>b;
             for(int i=0;i<b; i++){
-                x = x + dx[2];
-                y = y + dy[2];
+                x = x + dx[0];
+                y = y + dy[0];
             }
         }
-        else if(a=='E'){
+        else if(a=='S'){
             cin>>b;
             for(int i=0;i<b; i++){
                 x = x + dx[3];
                 y = y + dy[3];
             }
         }
+        else if(a=='W'){
+            cin>>b;
+            for(int i=0;i<b; i++){
+                x = x + dx[2];
+                y = y + dy[2];
+            }
+        }
     }
-    cout<<y<<" "<<x;
+    cout<<x<<" "<<y;
     return 0;
 }
