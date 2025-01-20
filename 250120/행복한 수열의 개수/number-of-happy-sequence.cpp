5f3c4c988fa3e41824a2b46afe1e0ca561ade8d6
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2025-01-20 18:01:21 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int grid[100][100];
+int index[101];
+
+int main() {
+    cin >> n >> m;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j == 0) {
+                index[grid[i][j]]++;
+            }
+            else {
+                if (grid[i][j - 1] == grid[i][j]) {
+                    index[grid[i][j]]++;
+                }
+                else {
+                    index[grid[i][j]] = 0;
+                    index[grid[i][j]]++;
+                }
+            }
+        }
+        int temp = 1;
+        for (int k = 0; k <= 100; k++) {
+            if (temp < index[k]) {
+                temp = index[k];
+            }
+        }
+        if (temp >= m) {
+            ans++;
+        }
+        
+        fill(index, index + 101, 0);
+    }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                index[grid[i][j]]++;
+            }
+            else {
+                if (grid[i - 1][j] == grid[i][j]) {
+                    index[grid[i][j]]++;
+                }
+                else {
+                    index[grid[i][j]] = 0;
+                    index[grid[i][j]]++;
+                }
+
+            }
+        }
+        int temp = 0;
+        for (int k = 0; k <= 100; k++) {
+            if (temp < index[k]) {
+                temp = index[k];
+            }
+        }
+        if (temp >= m) {
+            ans++;
+        }
+        fill(index, index + 101, 0);
+
+    }
+    
+    cout << ans;
+    return 0;
+}
