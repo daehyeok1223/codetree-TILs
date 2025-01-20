6a3aae4f688b78e6c180130f55c9499217e39d89
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2025-01-20 18:18:14 +0900

    Title: 행복한 수열의 개수, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250120/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250120/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -11,49 +11,67 @@ int main() {
     cin >> n >> m;
     int ans = 0;
 
-    // 입력 받기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
         }
     }
-
-    // 수평 방향 탐색
     for (int i = 0; i < n; i++) {
-        fill(index, index + 101, 0);
-        int temp = 0;
         for (int j = 0; j < n; j++) {
-            if (j > 0 && grid[i][j] == grid[i][j - 1]) {
+            if (j == 0) {
                 index[grid[i][j]]++;
             }
             else {
-                index[grid[i][j]] = 1; // 새로 시작
+                if (grid[i][j - 1] == grid[i][j]) {
+                    index[grid[i][j]]++;
+                }
+                else {
+                    index[grid[i][j]] = 1;
+                }
             }
-            temp = max(temp, index[grid[i][j]]);
         }
-        if (temp >= m) {
-            ans++;
-        }
-    }
-
-    // 수직 방향 탐색
-    for (int j = 0; j < n; j++) {
-        fill(index, index + 101, 0);
         int temp = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && grid[i][j] == grid[i - 1][j]) {
-                index[grid[i][j]]++;
+        for (int k = 0; k <= 100; k++) {
+            if (temp < index[k]) {
+                temp = index[k];
             }
-            else {
-                index[grid[i][j]] = 1; // 새로 시작
-            }
-            temp = max(temp, index[grid[i][j]]);
         }
         if (temp >= m) {
             ans++;
         }
+        
+        fill(index, index + 101, 0);
     }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                index[grid[i][j]]++;
+            }
+            else {
+                if (grid[i - 1][j] == grid[i][j]) {
+                    index[grid[i][j]]++;
+                }
+                else {
+                    index[grid[i][j]] = 1;
+                }
 
+            }
+        }
+        int temp = 0;
+        for (int k = 0; k <= 100; k++) {
+            if (temp < index[k]) {
+                temp = index[k];
+            }
+        }
+        if (temp >= m) {
+            ans++;
+        }
+        fill(index, index + 101, 0);
+
+    }
+    
     cout << ans;
     return 0;
 }
+
+
