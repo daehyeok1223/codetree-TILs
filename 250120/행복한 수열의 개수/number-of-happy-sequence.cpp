8a838aa74fea9698e5f621648c6b0cd1a342984a
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2025-01-20 18:17:08 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250120/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250120/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -7,49 +7,52 @@ int n, m;
 int grid[100][100];
 int index[101];
 
-// 연속된 숫자를 확인하는 함수
-int countConsecutive(int n, int m, bool isRow) {
-    int result = 0;
-    for (int i = 0; i < n; i++) {
-        fill(index, index + 101, 0);
-        int temp = 0;
-        for (int j = 0; j < n; j++) {
-            int value = isRow ? grid[i][j] : grid[j][i];
-            if (j > 0) {
-                int prev = isRow ? grid[i][j - 1] : grid[j - 1][i];
-                if (value == prev) {
-                    index[value]++;
-                } else {
-                    index[value] = 1; // 새로 시작
-                }
-            } else {
-                index[value] = 1;
-            }
-            temp = max(temp, index[value]);
-        }
-        if (temp >= m) {
-            result++;
-        }
-    }
-    return result;
-}
-
 int main() {
     cin >> n >> m;
+    int ans = 0;
 
+    // 입력 받기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
         }
     }
 
-    int ans = 0;
-
     // 수평 방향 탐색
-    ans += countConsecutive(n, m, true);
+    for (int i = 0; i < n; i++) {
+        fill(index, index + 101, 0);
+        int temp = 0;
+        for (int j = 0; j < n; j++) {
+            if (j > 0 && grid[i][j] == grid[i][j - 1]) {
+                index[grid[i][j]]++;
+            }
+            else {
+                index[grid[i][j]] = 1; // 새로 시작
+            }
+            temp = max(temp, index[grid[i][j]]);
+        }
+        if (temp >= m) {
+            ans++;
+        }
+    }
 
     // 수직 방향 탐색
-    ans += countConsecutive(n, m, false);
+    for (int j = 0; j < n; j++) {
+        fill(index, index + 101, 0);
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && grid[i][j] == grid[i - 1][j]) {
+                index[grid[i][j]]++;
+            }
+            else {
+                index[grid[i][j]] = 1; // 새로 시작
+            }
+            temp = max(temp, index[grid[i][j]]);
+        }
+        if (temp >= m) {
+            ans++;
+        }
+    }
 
     cout << ans;
     return 0;
