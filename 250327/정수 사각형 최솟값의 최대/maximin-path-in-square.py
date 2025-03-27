a4bb1f011b93055c7380a744e7583111aebe4128
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2025-03-27 16:52:34 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 5040ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 250327/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 250327/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -2,12 +2,12 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-def find_min():
-    pass
-
 def dfs(i, j, m):
-    global ans
-    if ans >= grid[i][j]:
+    global ans, v
+    if grid[i][j] <= ans:
+        return
+
+    if m <= v[i][j]:
         return
 
     m = min(m, grid[i][j])
@@ -15,9 +15,14 @@ def dfs(i, j, m):
         ans = max(ans, m)
         return
 
-    if i+1 < n: dfs(i+1, j, m)
-    if j+1 < n: dfs(i, j+1, m)
+    if i+1 < n:
+        dfs(i+1, j, m)
+        v[i+1][j] = ans
+    if j+1 < n:
+        dfs(i, j+1, m)
+        v[i][j+1] = ans
 
 ans = 0
+v = [[-1] * n for _ in range(n)]
 dfs(0, 0, 1000000)
 print(ans)
